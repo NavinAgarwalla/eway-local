d4fed06de0b5a4b706ff3e40d9043dea94f0b15d
Navin <navinagarwalla888@gmail.com>
2018-07-02 15:21:14 +0530

    thoda change

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function findStateByName(name) {
 
 
 function getStateInformations(req, res) {
-  console.log(req.body.conversation.memory.state);
+  console.log(req.body.conversation.memory);
   const state = req.body.conversation.memory.state.value;
   const stateInfos = findStateByName(state);
   if (!stateInfos) {

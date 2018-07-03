8718a5e61a0bf3c12388919068d98686fc4e3a55
Navin <navinagarwalla888@gmail.com>
2018-07-03 12:47:15 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,6 +1,124 @@
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
-//const contactinfo = require('./file.js');
+const db = require('./data.json');
+
+
+
+function findstateByName(state) {
+  const data = db.find(p => p.state.toLowerCase() === state.toLowerCase());
+  if (!data) {
+    return null;
+  }
+  return data;
+};
+
+
+
+
+
+
+function getstateInformations(req, res) {
+  const state = req.body.conversation.memory.state;
+  const stateInfos = findstateByName(state.value);
+   const memory = req.body.conversation.memory;
+  
+  console.log(memory);
+
+ 
+
+  if (!stateInfos) {
+    res.json({
+      replies: [
+        { type: 'text', content: `I don't know a state called ${state} :(` },
+      ],
+    });
+  } else {
+    res.json({
+      replies: [
+        { type: 'text', content: stateInfos.Mobile },
+        { type: 'text', content: stateInfos.Mail },
+        
+      ],
+    });
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+const app = express();
+app.use(bodyParser.json());
+
+// Load routes
+app.post('/state-informations', getstateInformations);
+//app.post('/pokemon-evolutions', getPokemonEvolutions);
+app.post('/errors', function (req, res) {
+  console.error(req.body);
+  res.sendStatus(200);
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+ 
+  console.log(`App is listening on port ${PORT}`);});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const express = require('express');
+const bodyParser = require('body-parser');
 const db = require('./data.json');
 
 
@@ -22,7 +140,7 @@ function getStateInformations(req, res) {
   const state = req.body.conversation.memory.state;
 
   const memory = req.body.conversation.memory;
- // const stateInfos = findStateByName(state.value);
+ const stateInfos = findStateByName(state.value);
   console.log(state);
   console.log(memory);
   if (!stateInfos) {
@@ -61,30 +179,4 @@ app.listen(PORT, () => {
 
 
 
-
-
-/*
-const app = express();
-app.use(bodyParser.json());
-
-const port=process.env.PORT|| 3000;
-
-app.post('/errors', (req, res) => {
-   console.error(req.body);
-   res.sendStatus(200); 
-});
-
-
-
-app.post('/contact-info', (req, res) => {
-    console.log('[POST] /contact-info');
-    const memory = req.body.conversation.memory;
-    const state= memory.state;
-    const district = memory.district;
-    contactinfo(state,district).then((data)=>{
-        res.json({replies:data});
-    });
-});
-
-app.listen(port, () => console.log(`App started on port ${port}`));
 */
